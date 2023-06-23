741ff5909f0bd6fff8f386a23ed3f390d2029d49
deloe <deloe@LAPTOP-57DIHML7>
2023-06-23 16:19:53 -0600

    proyecto final

---
+++ src/G4S21proyect3jom/g4s21proyect3jom.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package G4S21proyect3jom;
+
+/**
+ *
+ * @author GAARA
+ */
+public class g4s21proyect3jom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      new JFPrincipal().setVisible(true);
+    }
+    
+}
